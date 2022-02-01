e001b01be97cb112cb832362a60c6bbfd6e61a8d
Jhonatan Smith <jsjhonatanjsarroyo@gmail.com>
2022-02-01 11:58:57 -0500

    - Remplace "" por / en el comprobador del texto.
    - Cambie la parte del texto que dice Mensaje encriptado.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function encriptar() {
-  titulo.innerHTML = "Mensaje encriptado"
   let cambiar = textTarea.value;
-  let buscar = cambiar.search("^[a-z\u00f1\n ]+$");
+  let buscar = cambiar.search(/^[a-z\u00f1\s]+$/);
   let numero_0 = 0;
   let numero_1 = 1;
 
@@ -12,16 +11,17 @@ function encriptar() {
       numero_1 = numero_1 + 2;
     }
     cambiar = cambiar.toLowerCase();
+    titulo.innerHTML = "Mensaje encriptado";
     textTarea_2.value = cambiar;
   } else {
-    alerta(470, 3000, "error", "Solo se permite letras en minúsculas")
+    alerta(470, 3000, "error", "Solo se permite letras en minúsculas");
+    titulo.innerHTML = "Mensaje";
   }
 }
 
 function desencriptar() {
-  titulo.innerHTML = "Mensaje desencriptado"
   let cambiar = textTarea.value;
-  let buscar = cambiar.search("^[a-z\u00f1\n ]+$");
+  let buscar = cambiar.search(/^[a-z\u00f1\s]+$/);
   let numero_0 = 0;
   let numero_1 = 1;
 
@@ -32,26 +32,28 @@ function desencriptar() {
       numero_1 = numero_1 + 2;
     }
     cambiar = cambiar.toLowerCase();
+    titulo.innerHTML = "Mensaje desencriptado";
     textTarea_2.value = cambiar;
   } else {
-    alerta(470, 3000, "error", "Solo se permite letras en minúsculas")
+    alerta(470, 3000, "error", "Solo se permite letras en minúsculas");
+    titulo.innerHTML = "Mensaje";
   }
 }
 
 function copio() {
   let contenido = textTarea_2.value;
-  navigator.clipboard.writeText(contenido)
-  .catch(() => {
+  navigator.clipboard.writeText(contenido).catch(() => {
     nube = textTarea_2.value;
     textTarea_2.select();
     document.execCommand("copy");
     pegar.focus();
   });
-  alerta(210, 1500, "success", "Copiado")
+  alerta(210, 1500, "success", "Copiado");
 }
 
 function pego() {
-  navigator.clipboard.readText()
+  navigator.clipboard
+    .readText()
     .then((almacen) => (textTarea.value = almacen))
     .catch(() => {
       textTarea.value = nube;
@@ -59,41 +61,46 @@ function pego() {
 }
 
 function borrado() {
-  
   textTarea.value = "";
-
 }
 
 function borrado1() {
-  
   textTarea_2.value = "";
-
 }
 
 function alerta(tamaño, tiempo, icono, palabra) {
-  
   const Toast = Swal.mixin({
-  toast: true,
-  width: tamaño,
-  position: 'top',
-  showConfirmButton: false,
-  timer: tiempo,
-  timerProgressBar: true,
-  didOpen: (toast) => {
-    toast.addEventListener('mouseenter', Swal.stopTimer)
-    toast.addEventListener('mouseleave', Swal.resumeTimer)
-  }
-})
+    toast: true,
+    width: tamaño,
+    position: "top",
+    showConfirmButton: false,
+    timer: tiempo,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
 
-Toast.fire({
-  icon: icono,
-  title: palabra
-})
-
+  Toast.fire({
+    icon: icono,
+    title: palabra,
+  });
 }
 
 let vocales = ["a", "AI", "e", "ENTER", "i", "IMES", "o", "OBER", "u", "UFAT"];
-let vocales_1 = ["ai", "A", "enter", "E", "imes", "I", "ober", "O", "ufat", "U"];
+let vocales_1 = [
+  "ai",
+  "A",
+  "enter",
+  "E",
+  "imes",
+  "I",
+  "ober",
+  "O",
+  "ufat",
+  "U",
+];
 
 let nube;
 
@@ -116,8 +123,8 @@ pegar.onclick = pego;
 
 let borrar = document.querySelector("#borrar");
 let borrar1 = document.querySelector("#borrar1");
-borrar.onclick = borrado
-borrar1.onclick = borrado1
+borrar.onclick = borrado;
+borrar1.onclick = borrado1;
 
 /*
   La letra "e" es convertida para "enter"
